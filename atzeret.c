0b4f8d216f37d7a9349bb517e28fe80cda66393b
nasimdaghash <87161661+nasimdaghash@users.noreply.github.com>
2022-03-15 23:43:16 +0200

    Recorsion in C languege

---
+++ atzeret.c
@@ -0,0 +1,26 @@
+/* find the multiple from 1 to the n number */ 
+#include <stdio.h>  
+int fact (int);  
+int main()  
+{  
+    int n,f;  
+    printf("Enter the number whose factorial you want to calculate?");  
+    scanf("%d",&n);  
+    f = fact(n);  
+    printf("factorial = %d",f);  
+}  
+int fact(int n)  
+{  
+    if (n==0)  
+    {  
+        return 0;  
+    }  
+    else if ( n == 1)  
+    {  
+        return 1;  
+    }  
+    else   
+    {  
+        return n*fact(n-1);  
+    }  
+}  
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+ find the sum from 1 to a given number */
